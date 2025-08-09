917ca24af4ba36dcb1d3fa7af50e6cd440e95019
澤田祐紀 <>
2025-08-10 02:11:13 +0900

    test8

--- src/data/quiz.js
+++ src/data/quiz.js
@@ -2,12 +2,12 @@ const quizData = [
   {
     question: "Question1",
     options: ["Select 1", "Select 2", "Select 3", "Select 4"],
-    answerIndex: 1
+    answerIndex: 0
   },
   {
     question: "Question2",
     options: ["Select 1", "Select 2", "Select 3", "Select 4"],
-    answerIndex: 2
+    answerIndex: 1
   },
   {
     question: "Question3",
